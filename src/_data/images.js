a206e1aa70e29a02cfe5a236624b7d06d1b02cda
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-14 12:21:41 -0700

    feat: add image sizes

--- src/_data/images.js
+++ src/_data/images.js
@@ -1,10 +1,19 @@
 const fs = require("fs");
 const path = require("path");
+const sizeOf = require("image-size");
+
+const BASE_PATH = path.join(__dirname, "../../images/");
 
 module.exports = () => {
-	const images = fs
-		.readdirSync(path.join(__dirname, "../../images/"))
-		.map((image) => path.join("images", image));
+	const images = fs.readdirSync(BASE_PATH).map((image) => {
+		const { width, height } = sizeOf(path.join(BASE_PATH, image));
+
+		return {
+			path: path.join("images", image),
+			width,
+			height,
+		};
+	});
 
 	return images;
 };
